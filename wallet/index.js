bf740bd5d445b64eff2a23bf0c7a82e6406ed19a
Andrey Khalepov <w54661c@gmail.com>
2023-10-01 15:29:47 +1000

    wallet done

--- wallet/index.js
+++ wallet/index.js
@@ -2,12 +2,18 @@ const wallet = {
     balance: 0,
     operations: [],
     income: function (sum, reason) {
+        this.balance += sum;
         this.operations.push({
             reason,
             sum,
         });
+        return true;
     },
     spend: function (sum, reason) {
+        if (sum > this.balance) {
+            return false;
+        }
+        this.balance -= sum;
         this.operations.push({
             reason,
             sum: -sum,
