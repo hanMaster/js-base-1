59343088792b80db2c2ab71588635147f081fab8
Andrey Khalepov <w54661c@gmail.com>
2023-10-06 23:50:26 +1000

    form

--- final-project/assets/js/index.js
+++ final-project/assets/js/index.js
@@ -5,6 +5,14 @@ import { loadData } from './utils.js';
 
 let habbits = [];
 
+const form = document.querySelector('.habbit__form');
+
+form.onsubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log(data.get('comment'));
+};
+
 (() => {
     habbits = loadData();
     rerender(habbits[0].id, habbits);
